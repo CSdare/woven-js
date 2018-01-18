ba8689014bc30b395a3cdb713a49dd1f41524620
Erik Larsen <erik.w.larsen@gmail.com>
2018-01-17 21:57:17 -0800

    add initial optimize function

---
+++ src/optimize.js
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ */
+
+module.exports = function optimizeWrapper(options) {
+  return function optimize(req, res, next) {
+    
+    if (req.url === '/__woven_first__') {
+      // compare options from server with options on client side
+      // need to be more specific about which options are sent
+      options.defaults = false;
+      console.log('back end options before sending:', options);
+      res.json(options);
+    }
+
+
+    else if (req.url === '/__woven__') {
+      console.log('got to optimize');
+      const output = options.functions[req.body.funcName](req.body.payload);
+      res.json(output);
+    }
+    else next();
+  }
+}
+
