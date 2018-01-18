1fcf487ed91b6b6314f626c1372804955ce6e04e
Erik Larsen <erik.w.larsen@gmail.com>
2018-01-17 21:55:43 -0800

    add test files for woven.run implementation

---
+++ dev/testServer.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const woven = require('../index');
+
+app.use(bodyParser.json());
+// how can we incorporate bodyParser into our woven.optimize middleware?
+
+woven.configure(__dirname + '/testFunctions.js', { alwaysServer: true });
+
+app.use(woven.optimize);
+
+app.use(express.static(path.join(__dirname, '/dist/')));
+
+app.listen(3000);
