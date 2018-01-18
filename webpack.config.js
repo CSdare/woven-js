91ebed788e4e7dc4b1ce3e31bdca87b607ac893d
ryanlondon <ryel590@gmail.com>
2018-01-17 22:46:07 -0800

    added woven-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'webpack_test/app.js'),
+  entry: path.resolve(__dirname, 'dev/webpack_test/app.js'),
   output: {
-    path: path.resolve(__dirname, 'webpack_test'),
+    path: path.resolve(__dirname, 'dev/webpack_test'),
     filename: "bundle.js",
+  },
+  resolveLoader: {
+    modules: ['node_modules', path.resolve(__dirname, 'custom_loaders')]
   },
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: [ path.resolve(__dirname, 'dev/functions') ],
         use: [ 
           { loader: 'babel-loader' },
           { 
-            loader: path.resolve('./cat_loader/cat-loader.js'),
+            loader: 'cat-loader',
             options: { animal: 'kitten', saying: 'pet me' },
           }
         ],
+      },
+      {
+        test: /\.js$/,
+        include: [ path.resolve(__dirname, 'dev/functions') ],
+        loader: [ 'babel-loader', 'woven-loader' ],
       },
     ],
   },
