76201b4cf9852e2540f47d5475c06f4e37bae97e
Erik Larsen <erik.w.larsen@gmail.com>
2018-01-19 20:05:52 -0800

    comment out unfinished Pool.js file before npm publish

--- src/Pool.js
+++ src/Pool.js
@@ -1,18 +1,18 @@
-function Pool(size) {
-  this.taskQueue = [];
-  this.workerQueue = [];
-  this.poolSize = size;
+// function Pool(size) {
+//   this.taskQueue = [];
+//   this.workerQueue = [];
+//   this.poolSize = size;
 
 
-}
+// }
 
-function WorkerThread(pool) {
-  this.pool = pool;
-  this.workerTask = {};
+// function WorkerThread(pool) {
+//   this.pool = pool;
+//   this.workerTask = {};
 
-  this.run = function(workerTask) {
-    this.workerTask = workerTask;
-  }
-}
+//   this.run = function(workerTask) {
+//     this.workerTask = workerTask;
+//   }
+// }
 
-function WorkerTask(funcName, )
+// function WorkerTask(funcName, )
