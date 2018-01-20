a55b142170f708cb4b49c44b47644f8e5371e784
Erik Larsen <erik.w.larsen@gmail.com>
2018-01-20 11:13:08 -0800

    import Pool and WorkerTask

--- client.js
+++ client.js
@@ -2,7 +2,9 @@ console.log('hello from woven-client');
 
 const options = require('./src/options');
 const optimal = require('./src/optimal');
-const connect = require('./src/connect')(options);
-const run = require('./src/run')(options, optimal);
+const Pool = require('./src/Pool').Pool; // does this need to reference options object? only time will tell
+const WorkerTask = require('./src/Pool').WorkerTask;
+const connect = require('./src/connect')(options, optimal, Pool);
+const run = require('./src/run')(options, optimal, WorkerTask);
 
-module.exports = { connect, run };
+module.exports = { connect, run, Pool, WorkerTask };
