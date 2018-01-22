062e30f790695e601094a19dfb9ba7378c0d003d
Erik Larsen <erik.w.larsen@gmail.com>
2018-01-21 22:09:04 -0800

    fix configure function to pass in optimal

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ console.log('hello from woven');
 const options = require('./src/options');
 const optimal = require('./src/optimal');
 const configure = require('./src/configure')(options, optimal);
-const optimize = require('./src/optimize')(options);
+const optimize = require('./src/optimize')(options, optimal);
 
 module.exports = { configure, optimize };
