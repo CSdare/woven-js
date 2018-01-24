011b576644c91b2d0cdfcb2c3e0c6abdf74b8d31
Erik Larsen <erik.w.larsen@gmail.com>
2018-01-24 12:05:32 -0800

    remove responseMin, change dynamicMin to dynamicMax

--- src/options.js
+++ src/options.js
@@ -1,11 +1,10 @@
 module.exports = {
   alwaysClient: false,
   alwaysServer: false,
-  dynamicMin: 500,
+  dynamicMax: 500,
   fallback: 'server',
   functions: null,
   maxThreads: 12,
   pingSize: 100,
-  responseMin: 500,
   stringPing: null,
 }
