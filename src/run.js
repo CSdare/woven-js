b27968ff784039d94dc2cc04e7eda075303533cc
Erik Larsen <erik.w.larsen@gmail.com>
2018-02-09 18:03:16 -0800

    lint run.js and add errorCallback for worker - test this

--- src/run.js
+++ src/run.js
@@ -1,35 +1,30 @@
 /**
- * 
+ *
  * @param {Object} optimal - optimal computing location (optimal.location) and thread number 
  *                     (optimal.threads) based on client optimization function
  */
 
 module.exports = function runWrapper(optimal, WorkerTask) {
- 
-  /**
-   * @param {string} funcName: string referring to name of a function in functions.js
-   * @param {Object} payload: arguments to be passed into the function
-   */
-  
   return function run(funcName, ...payload) {
+    // use Web Worker pool if processing will be done on client
     if (optimal.location === 'client') {
-
       return new Promise((resolve, reject) => {
         function workerCallback(output) {
           resolve(output);
         }
-        const workerTask = new WorkerTask(funcName, payload, workerCallback);
-
+        function errorCallback(error) {
+          reject(error);
+        }
+        const workerTask = new WorkerTask(funcName, payload, workerCallback, errorCallback);
         optimal.pool.addWorkerTask(workerTask);
-
       });
+    // POST request to server if processing will be done on server
     } else if (optimal.location === 'server') {
       return fetch('/__woven__', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ funcName, payload }),
-      })
-      .then(res => res.json())
+      }).then(res => res.json());
     }
-  }
-}
+  };
+};
