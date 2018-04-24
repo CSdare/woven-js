445c03d7d287beeaffef2aa7507f7791c1444ebc
Erik Larsen <erik.w.larsen@gmail.com>
2018-04-24 02:17:26 -0400

    (update) add default options for child processes

--- src/options.js
+++ src/options.js
@@ -1,10 +1,15 @@
+const os = require('os');
+
 module.exports = {
   alwaysClient: false,
   alwaysServer: false,
   dynamicMax: 500,
   fallback: 'server',
   functions: null,
-  maxThreads: 12,
+  maxChildThreads: os.cpus().length,
+  maxWorkerThreads: 12,
   pingSize: 100,
   stringPing: null,
+  useChildProcess: true,
+  useWebWorkers: true,
 };
